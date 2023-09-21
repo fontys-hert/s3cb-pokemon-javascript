cc267f60373eaaecf5ab9b5e854b2f79cf4f25f8
Timo Hermans <tmhermans@gmail.com>
2023-09-21 12:07:00 +0200

    Base challenge setup

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function setStartingHealth(target, hp) {
+  const currentHealthEl = document.querySelector(`#${target} #currentHealth`);
+  const maxHealthEl = document.querySelector(`#${target} #maxHealth`);
+
+  currentHealthEl.innerHTML = maxHealthEl.innerHTML = hp;
+}
+
+setStartingHealth("player", 30);
+setStartingHealth("enemy", 28);
